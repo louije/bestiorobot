1425b86c21fc8279445d99036b5eda4f422b1d8c
Louis-Jean Teitelbaum <ljt@meidosem.com>
2023-04-21 16:48:35 +0200

    refactor to Board.svelte

---
+++ src/Board.svelte
@@ -0,0 +1,140 @@
+<script lang="ts">
+  import { onMount } from "svelte";
+  import { fragmentPath, soundFileFor } from "$lib/base";
+
+  export let phrase: string;
+  export let file: () => Promise<any>;
+  
+  let loadingIteration = 0;
+
+  let root: HTMLElement;
+  let boardComponent: any;
+  let svg: SVGSVGElement | null;
+  let fragments: Array<SVGPathElement>;
+  let sounds: Array<HTMLAudioElement>;
+
+  onMount(async () => {
+    const module = await file();
+    boardComponent = module.default;
+    requestAnimationFrame(waitForSVG);
+  });
+
+  function waitForSVG() {
+    svg = root.querySelector("svg");
+    if (!svg) {
+      loadingIteration += 1;
+      if (loadingIteration > 16) {
+        return;
+      }
+      return requestAnimationFrame(waitForSVG);
+    }
+    prosodize();
+  }
+
+  function prosodize() {
+    cleanupSVG();
+    getFragments();
+    loadSounds();
+    addEvents();
+  }
+
+  function cleanupSVG() {
+    // do stuff with viewBox, etc.
+  }
+
+  function getFragments() {
+    if (!svg) {
+      return;
+    }
+    const fragmentElements = [
+      ...svg.querySelectorAll<SVGPathElement>("#fragments path, #FRAGMENTS path")
+    ];
+    fragmentElements.forEach((f) => {
+      f.id = f.id.replaceAll("_x5F", "").replaceAll("__", "_").replace(/_\d{4,}_$/, "");
+    });
+    fragments = fragmentElements.sort((a: SVGPathElement, b: SVGPathElement) => {
+      return getPathX(a) - getPathX(b);
+    });
+  }
+
+  function loadSounds() {
+    const [monster, phraseNum, child] = phrase.split("_");
+    const useIDs = true; //isNaN(parseInt(child));
+    let soundPaths: Array<string> = [];
+
+    if (useIDs) {
+      soundPaths = fragments.map((f) => {
+        const id = f.id.replaceAll("_x5F", "");
+        return soundFileFor(id);
+      });
+    } else {
+      soundPaths = fragments.map((f, i) => {
+        return fragmentPath(monster, phraseNum, child, i + 1);
+      });
+    }
+
+    sounds = soundPaths.map((p, idx) => {
+      const audio = document.createElement("audio") as HTMLAudioElement;
+      audio.src = p;
+      audio.id = `audio_${idx}`;
+      audio.addEventListener("canplaythrough", (e) => {
+        console.log("loaded", e.target, e);
+      });
+      audio.hidden = true;
+      root.append(audio);
+      return audio;
+    });
+  }
+
+  function addEvents() {
+    fragments.forEach((f, i) => {
+      f.addEventListener("click", () => {
+        console.log(i);
+        sounds[i].play();
+      });
+      f.addEventListener("mouseenter", () => {
+        console.log(i);
+        sounds[i].play();
+      });
+    });
+  }
+
+  function getPathX(path: Element): number {
+    const shape = path.getAttribute("d");
+    if (!shape) {
+      return 0;
+    }
+    return parseInt(shape.split(",")[0].replace("M", ""));
+  }
+</script>
+
+<div class="root" bind:this={root}>
+  <svelte:component this={boardComponent} />
+</div>
+
+<style global>
+  .root {
+    display: flex;
+    width: 100%;
+    height: 100%;
+  }
+  :global(.root svg) {
+    width: 100%;
+  }
+
+  :global(#AXE path),
+  :global(#GRILLE path),
+  :global(#grille path) {
+    stroke: #f96806;
+    stroke-width: 3px;
+  }
+  :global(#FRAGMENTS path),
+  :global(#fragments path) {
+    cursor: pointer;
+    opacity: 0.8;
+  }
+  :global(#FRAGMENTS path:hover),
+  :global(#fragments path:hover) {
+    opacity: 1;
+  }
+</style>
--- src/routes/prosodie/[phrase]/+page.svelte
+++ src/routes/prosodie/[phrase]/+page.svelte
@@ -1,142 +1,18 @@
 <script lang="ts">
-  import { onMount } from "svelte";
+  import Board from "@/Board.svelte";
   import type { PageData } from "./$types";
-  import { fragmentPath, soundFileFor } from "$lib/base";
-
   export let data: PageData;
-  const phrase = data.phrase;
-  const board = data.file;
-  let loadingIteration = 0;
-
-  // const board = () => import(`../../../phrases/${phrase}.svg`);
-
-  let root: HTMLElement;
-  let boardComponent: any;
-  let svg: SVGSVGElement | null;
-  let fragments: Array<SVGPathElement>;
-  let sounds: Array<HTMLAudioElement>;
-
-  onMount(async () => {
-    const module = await board();
-    boardComponent = module.default;
-    requestAnimationFrame(waitForSVG);
-  });
-
-  function waitForSVG() {
-    svg = root.querySelector("svg");
-    if (!svg) {
-      loadingIteration += 1;
-      if (loadingIteration > 16) {
-        return;
-      }
-      return requestAnimationFrame(waitForSVG);
-    }
-    prosodize();
-  }
-
-  function prosodize() {
-    getFragments();
-    loadSounds();
-    addEvents();
-  }
-
-  function getFragments() {
-    if (!svg) {
-      return;
-    }
-    const fragmentElements = [
-      ...svg.querySelectorAll<SVGPathElement>("#fragments path, #FRAGMENTS path")
-    ];
-    fragmentElements.forEach((f) => {
-      f.id = f.id.replaceAll("_x5F", "").replace(/_\d{4,}_$/, "");
-    });
-    fragments = fragmentElements.sort((a: SVGPathElement, b: SVGPathElement) => {
-      return getPathX(a) - getPathX(b);
-    });
-  }
-
-  function loadSounds() {
-    const [monster, phraseNum, child] = phrase.split("_");
-    const useIDs = true; //isNaN(parseInt(child));
-    let soundPaths: Array<string> = [];
-
-    if (useIDs) {
-      soundPaths = fragments.map((f) => {
-        const id = f.id.replaceAll("_x5F", "");
-        return soundFileFor(id);
-      });
-    } else {
-      soundPaths = fragments.map((f, i) => {
-        return fragmentPath(monster, phraseNum, child, i + 1);
-      });
-    }
-
-    sounds = soundPaths.map((p, idx) => {
-      const audio = document.createElement("audio") as HTMLAudioElement;
-      audio.src = p;
-      audio.id = `audio_${idx}`;
-      audio.addEventListener("canplaythrough", (e) => {
-        console.log("loaded", e.target, e);
-      });
-      audio.hidden = true;
-      root.append(audio);
-      return audio;
-    });
-  }
-
-  function addEvents() {
-    fragments.forEach((f, i) => {
-      f.addEventListener("click", () => {
-        console.log(i);
-        sounds[i].play();
-      });
-      f.addEventListener("mouseenter", () => {
-        console.log(i);
-        sounds[i].play();
-      });
-    });
-  }
-
-  function getPathX(path: Element): number {
-    const shape = path.getAttribute("d");
-    if (!shape) {
-      return 0;
-    }
-    return parseInt(shape.split(",")[0].replace("M", ""));
-  }
 </script>
 
 <main>
-  <div class="root" bind:this={root}>
-    <svelte:component this={boardComponent} />
-  </div>
+  <Board phrase={data.phrase} file={data.file} />
 </main>
 
 <style>
   main {
-    width: 80vw;
-    height: calc(80vw / (5 / 3));
-    margin: 2rem auto;
+    width: 100vw;
+    height: 100vh;
     display: flex;
   }
-  .root {
-    display: flex;
-    width: 100%;
-    height: 100%;
-  }
-  :global(#AXE path),
-  :global(#GRILLE path),
-  :global(#grille path) {
-    stroke: #f96806;
-    stroke-width: 3px;
-  }
-  :global(#FRAGMENTS path),
-  :global(#fragments path) {
-    cursor: pointer;
-    opacity: 0.8;
-  }
-  :global(#FRAGMENTS path:hover),
-  :global(#fragments path:hover) {
-    opacity: 1;
-  }
+
 </style>
