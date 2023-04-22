f25ae2c14dad1726665ccb84759e762e6395d5c5
Louis-Jean Teitelbaum <ljt@meidosem.com>
2023-04-22 10:57:21 +0200

    cleaner index

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,51 +4,51 @@
   // import AudioStarter from "./AudioStarter.svelte";
 
   export let data: PageData;
+  
+  const levels = Object.keys(data.phrasesPerLevel);
 </script>
 
 <main>
   <!-- <AudioStarter> -->
     <div class="root">
-      {#each data.phrases as phrase}
-        <a href="/prosodie/{phrase}">{phrase}</a>
+      <h1>Tableaux</h1>
+      {#each levels as level}
+        <h2>Niveau {level}</h2>
+        <ul>
+          {#each data.phrasesPerLevel[level] as phrase}
+            <li><a href="/prosodie/{phrase}">{phrase}</a></li>
+          {/each}
+        </ul>
       {/each}
     </div>
   <!-- </AudioStarter> -->
 </main>
 
 <style>
-  main {
-    width: 50rem;
-    height: 31rem;
-    margin: 2rem auto;
-    display: flex;
-    font-family: sans-serif;
-    font-size: 2rem;
-  }
-  .root {
-    display: flex;
-    width: 100%;
-    height: 100%;
-    align-items: center;
-    justify-content: center;
-    flex-direction: column;
-  }
-  a {
-    display: block;
-  }
-  :global(#GRILLE path),
-  :global(#grille path) {
-    stroke: #f96806;
-    stroke-width: 3px;
-  }
-  :global(#FRAGMENTS path),
-  :global(#fragments path) {
-    cursor: pointer;
-    opacity: 0.8;
-    /* fill: #44C17A; */
-  }
-  :global(#FRAGMENTS path:hover),
-  :global(#fragments path:hover) {
-    opacity: 1;
-  }
+main {
+  padding: 3rem;
+  font-family: sans-serif;
+  color: #52b378;
+  max-width: 30rem;
+}
+h1 {
+  font-size: 2rem;
+}
+h2 {
+  font-size: 1.5rem;
+  margin-bottom: .5rem;
+  border-bottom: 1px solid #f37516;
+}
+ul {
+  list-style: none;
+  padding: 0;
+  margin: 0;
+}
+a:any-link {
+  color: inherit;
+  text-decoration: none;
+}
+li {
+  line-height: 1.25rem;
+}
 </style>
--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,10 +1,10 @@
 import { error } from "@sveltejs/kit";
 import type { PageLoad } from "./$types";
-import { phrases } from "@/lib/svgloader";
+import { phrases, phrasesPerLevel } from "@/lib/svgloader";
 
 export const load = (() => {
   if (phrases.length > 0) {
-    return { phrases };
+    return { phrases, phrasesPerLevel };
   }
   throw error(404, "Not found");
 }) satisfies PageLoad;
